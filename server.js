d72de19150f20ea3d15741d3814439be51c5f2dc
Neo Marvel <name.milton@gmail.com>
2019-01-10 19:01:52 +0530

    initial commit
    server and client files, with package.json

---
+++ server.js
@@ -0,0 +1,13 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+    port: 1337
+});
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        console.log('received: %s', message);
+    });
+
+    ws.send('Odaruthammavaa aalariyaam!!');
+});
